82e34c22837789989fa92dcb724cb617a9e0fbba
unknown <kennykarnama@gmail.com>
2022-10-21 03:46:10 +0700

    add migration

---
+++ apps/message-api/src/main/java/com/rakutenmobile/messageapi/usermessage/domain/exception/MessageNotFound.java
@@ -0,0 +1,7 @@
+package com.rakutenmobile.messageapi.usermessage.domain.exception;
+
+public class MessageNotFound extends Exception {
+    public MessageNotFound(String message) {
+        super(message);
+    }
+}
